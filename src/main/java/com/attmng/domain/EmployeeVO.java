49032009028b91de7870025395236e85f82b7caa
dndi6 <dndi6@192.168.1.134>
2019-05-27 12:42:58 +0900

    #ログイン機能VO追加

---
+++ src/main/java/com/attmng/domain/EmployeeVO.java
@@ -0,0 +1,24 @@
+package com.attmng.domain;
+
+
+/*
+ * table�걫�겏�겓DTO�굮鵝쒏닇�걮�겍�걦�걽�걬�걚��
+ */
+public class EmployeeVO {
+	private String id;
+    private String password;
+
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
