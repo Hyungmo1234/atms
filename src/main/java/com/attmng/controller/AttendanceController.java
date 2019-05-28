9da5e5f9e0a8cd0a027b0615d42e9b41c97b9c1a
HyeongSeokSeong <hyeongseok6778@gmail.com>
2019-05-28 19:02:17 +0900

    attendance part db connect & select check

---
+++ src/main/java/com/attmng/controller/AttendanceController.java
@@ -0,0 +1,51 @@
+package com.attmng.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.attmng.domain.AttendanceVO;
+import com.attmng.dto.AttendanceDTO;
+import com.attmng.service.AttendanceService;
+
+@Controller
+public class AttendanceController {
+
+	@Autowired
+    private AttendanceService AService;
+	
+
+	@RequestMapping(value = "/attendanceGET", method = RequestMethod.GET)
+	public String AttendanceGet(Model model, HttpServletResponse response, HttpServletRequest request, AttendanceDTO dto,HttpSession session) throws Exception {	
+		
+		List<AttendanceVO> attendance = AService.AttendanceGET("designer");
+		
+		model.addAttribute("attendanceList", attendance);
+		/* request.getAttribute(ID); */
+		return "test";
+		
+	}
+	
+	@RequestMapping(value = "/ExcelSave", method = RequestMethod.GET)
+	public String ExcelGet(Model model, HttpServletResponse response, HttpServletRequest request, AttendanceDTO dto,HttpSession session) throws Exception {	
+		
+		List<AttendanceVO> attendance = AService.ExcelGet("designer", "0");
+		
+		model.addAttribute("getExcelInfo", attendance);
+		/* request.getAttribute(ID); */
+		return "test";
+		
+	}
+	
+	
+	
+}
