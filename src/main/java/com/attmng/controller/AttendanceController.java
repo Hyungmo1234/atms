e811e4136050cbd3f4ddfe0cb9168d564ef97c9c
lcj225 <32259312+lcj225@users.noreply.github.com>
2019-05-29 20:04:49 +0900

    Update AttendanceController.java

--- src/main/java/com/attmng/controller/AttendanceController.java
+++ src/main/java/com/attmng/controller/AttendanceController.java
@@ -22,73 +22,73 @@ import com.attmng.service.AttendanceService;
 @Controller
 public class AttendanceController {
 
-	@Autowired
-	private AttendanceService AService;
-	
-	SimpleDateFormat format = new SimpleDateFormat ("yyyyMM");
-	String yearMonth = format.format (System.currentTimeMillis());
-	EmployeeVO vo;
+   @Autowired
+   private AttendanceService AService;
+   
+   SimpleDateFormat format = new SimpleDateFormat ("yyyyMM");
+   String yearMonth = format.format (System.currentTimeMillis());
+   EmployeeVO vo;
 
-	@RequestMapping(value = "/attendanceGET", method = RequestMethod.GET)
-	public String AttendanceGet(Model model, HttpServletResponse response, HttpServletRequest request,
-			AttendanceDTO dto, HttpSession session) throws Exception {
+   @RequestMapping(value = "/attendanceGET", method = RequestMethod.GET)
+   public String AttendanceGet(Model model, HttpServletResponse response, HttpServletRequest request,
+         AttendanceDTO dto, HttpSession session) throws Exception {
 
-		/*
-		 * String year = request.getParameter("year"); String month =
-		 * request.getParameter("month");
-		 * 
-		 * if(Integer.parseInt(month) < 10) { month = "0" + month; }
-		 * 
-		 * String yearMonth = year + month;
-		 */
-		
-		vo =  (EmployeeVO) session.getAttribute("Logininfo");
-	
-		List<AttendanceVO> attendance = AService.AttendanceGET(vo.getId(), yearMonth, 0);
+      /*
+       * String year = request.getParameter("year"); String month =
+       * request.getParameter("month");
+       * 
+       * if(Integer.parseInt(month) < 10) { month = "0" + month; }
+       * 
+       * String yearMonth = year + month;
+       */
+      
+      vo =  (EmployeeVO) session.getAttribute("Logininfo");
+   
+      List<AttendanceVO> attendance = AService.AttendanceGET(vo.getId(), yearMonth, 0);
 
-		model.addAttribute("attendanceList", attendance);
-		/* request.getAttribute(ID); */
-		return "G06-1";
+      model.addAttribute("attendanceList", attendance);
+      /* request.getAttribute(ID); */
+      return "G06-1";
 
-	}
-	
-	@RequestMapping(value = "/attendanceUpdate", method = RequestMethod.GET)
-	public String AttendanceUpdate(Model model, HttpServletResponse response, HttpServletRequest request,
-			AttendanceDTO dto, HttpSession session) throws Exception {
+   }
+   
+   @RequestMapping(value = "/attendanceUpdate", method = RequestMethod.GET)
+   public String AttendanceUpdate(Model model, HttpServletResponse response, HttpServletRequest request,
+         AttendanceDTO dto, HttpSession session) throws Exception {
 
-		vo =  (EmployeeVO) session.getAttribute("Logininfo");
-		
-		String year = request.getParameter("year"); 
-		String month = request.getParameter("month");
-		
-		if(Integer.parseInt(month) < 10) { 
-			month = "0" + month; 
-		}
-		
-		
-		String yearMonth = year + month;
-		
-		AService.AttendanceUpdate(vo.getId(), yearMonth, "12", 0);
+      vo =  (EmployeeVO) session.getAttribute("Logininfo");
+      
+      String year = request.getParameter("year"); 
+      String month = request.getParameter("month");
+      
+      if(Integer.parseInt(month) < 10) { 
+         month = "0" + month; 
+      }
+      
+      
+      String yearMonth = year + month;
+      
+      AService.AttendanceUpdate(vo.getId(), yearMonth, "12", 0);
 
-		List<AttendanceVO> attendance = AService.AttendanceGET(vo.getId(), yearMonth, 0);
+      List<AttendanceVO> attendance = AService.AttendanceGET(vo.getId(), yearMonth, 0);
 
-		model.addAttribute("attendanceList", attendance);
-		//model.addAttribute("attendanceUpdate", attendance);
-		/* request.getAttribute(ID); */
+      model.addAttribute("attendanceList", attendance);
+      //model.addAttribute("attendanceUpdate", attendance);
+      /* request.getAttribute(ID); */
 
-		return "G06-1";
-	}
-	/*
-	 * @RequestMapping(value = "/ExcelSave", method = RequestMethod.GET) public
-	 * String ExcelGet(Model model, HttpServletResponse response, HttpServletRequest
-	 * request, AttendanceDTO dto,HttpSession session) throws Exception {
-	 * 
-	 * List<AttendanceVO> attendance = AService.ExcelGet("designer", "0");
-	 * 
-	 * model.addAttribute("getExcelInfo", attendance); request.getAttribute(ID);
-	 * return "test";
-	 * 
-	 * }
-	 */
+      return "G06-1";
+   }
+   /*
+    * @RequestMapping(value = "/ExcelSave", method = RequestMethod.GET) public
+    * String ExcelGet(Model model, HttpServletResponse response, HttpServletRequest
+    * request, AttendanceDTO dto,HttpSession session) throws Exception {
+    * 
+    * List<AttendanceVO> attendance = AService.ExcelGet("designer", "0");
+    * 
+    * model.addAttribute("getExcelInfo", attendance); request.getAttribute(ID);
+    * return "test";
+    * 
+    * }
+    */
 
 }
